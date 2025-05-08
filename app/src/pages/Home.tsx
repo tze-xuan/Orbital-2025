8eacb2def47ec02f900ea86d906d200cf1f49f37
tze-xuan <ktzexuan@gmail.com>
2025-05-08 23:44:44 +0800

    Add home page

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Flex, Text, Button } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Flex justifyContent="center" alignItems="center" direction="column">
+      <Flex
+        direction="row"
+        width="100vw"
+        gap="200px"
+        justifyContent="center"
+        marginTop="25px"
+      >
+        {/* TODO TIDY UP BUTTONS W CONST */}
+        <Button
+          variant="link"
+          fontFamily="afacad"
+          fontSize="2xl"
+          fontWeight="medium"
+          color="#3E405B"
+          _hover={{ textColor: "#DC6739", textDecoration: "underline" }}
+        >
+          sidebar
+        </Button>
+        <Button
+          variant="link"
+          fontFamily="afacad"
+          fontSize="2xl"
+          fontWeight="medium"
+          color="#3E405B"
+          _hover={{ textColor: "#DC6739", textDecoration: "underline" }}
+        >
+          button1
+        </Button>
+        <Button
+          variant="link"
+          fontFamily="afacad"
+          fontSize="2xl"
+          fontWeight="medium"
+          color="#3E405B"
+          _hover={{ textColor: "#DC6739", textDecoration: "underline" }}
+        >
+          <a href="/community">Our Community</a>
+        </Button>
+        <Button
+          variant="link"
+          fontFamily="afacad"
+          fontSize="2xl"
+          fontWeight="medium"
+          color="#3E405B"
+          _hover={{ textColor: "#DC6739", textDecoration: "underline" }}
+        >
+          button3
+        </Button>
+        <Button
+          variant="link"
+          fontFamily="afacad"
+          fontSize="2xl"
+          fontWeight="medium"
+          color="#3E405B"
+          _hover={{ textColor: "#DC6739", textDecoration: "underline" }}
+        >
+          inbox
+        </Button>
+      </Flex>
+
+      <Text fontFamily="darumadrop" fontSize="8xl" color="#DC6739">
+        Café Chronicles
+      </Text>
+    </Flex>
+  );
+};
+
+export default Home;
