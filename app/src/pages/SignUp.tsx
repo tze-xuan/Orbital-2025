0e8f8b075ec72227ba740fce95e00c11ef9f4660
tze-xuan <ktzexuan@gmail.com>
2025-05-08 23:45:00 +0800

    Add sign up page

---
+++ app/src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+const SignUp = () => {
+  return (
+    <Flex className="home">
+      <Text>Sign Up Page</Text>
+    </Flex>
+  );
+};
+
+export default SignUp;
