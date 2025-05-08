0af5b795b3d76f70683e2ae2c02968b6e752d7f2
tze-xuan <ktzexuan@gmail.com>
2025-05-08 23:45:12 +0800

    Add community page

---
+++ app/src/pages/Community.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Flex, Text } from "@chakra-ui/react";
+
+const Community = () => {
+  return (
+    <Flex className="home">
+      <Text>Community Page</Text>
+    </Flex>
+  );
+};
+
+export default Community;
