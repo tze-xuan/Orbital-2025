e743066a75845f74421cc9d9d382b68f182bd0c7
tze-xuan <ktzexuan@gmail.com>
2025-06-08 17:26:46 +0800

    Add home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,42 @@
-import { Text } from "@chakra-ui/react";
+import { Flex, Button } from "@chakra-ui/react";
+import { NavButton } from "../components/Buttons.tsx";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <Flex height="100vh" width="100vw">
+      <Flex
+        direction="row"
+        bgColor="#FFCE58"
+        width="100vw"
+        height="14vh"
+        alignItems="center"
+        justifyContent="center"
+        gap="10vw"
+      >
+        <NavButton>button</NavButton>
+        <NavButton>button</NavButton>
+        <Button
+          variant="plain"
+          fontFamily="darumadrop"
+          color="#DC6739"
+          fontSize="3xl"
+        >
+          <a href="/">Café Chronicles</a>
+        </Button>
+        <NavButton>button</NavButton>
+        <NavButton
+          bgColor="#80B29B"
+          color="#FEF1C5"
+          borderRadius="50px"
+          border="1px"
+          shadow="md"
+          _hover={{ shadow: "dark-lg" }}
+        >
+          <a href="/login">Login</a>
+        </NavButton>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
